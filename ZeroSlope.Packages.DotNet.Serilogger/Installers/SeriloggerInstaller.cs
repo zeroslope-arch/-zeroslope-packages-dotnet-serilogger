59965bf90200505ea77cde9157bdb0af1ed8d7bf
cfryerdev <cfryerdev@gmail.com>
2021-11-17 15:12:38 -0700

    Initial commit

---
+++ ZeroSlope.Packages.DotNet.Serilogger/Installers/SeriloggerInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace ZeroSlope.Packages.DotNet.Serilogger.Installers
+{
+    public class SeriloggerInstaller
+    {
+		public SeriloggerInstaller()
+        {
+
+        }
+
+		public void Install(IServiceCollection serviceCollection)
+		{
+			var logger = new LoggerConfiguration()
+				.WriteTo
+				.Console()
+				.CreateLogger();
+			serviceCollection.AddSingleton<ILogger>(logger);
+		}
+	}
+}
